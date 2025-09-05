da7240bbbefcecc1c8a5b2ae0d134506fc5bc387
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-05 20:12:33 +0600

    some problem fixed and added condition

--- script/script.js
+++ script/script.js
@@ -20,7 +20,7 @@ const loadCategory = () => {
 const showCategory = (categories) => {
     categories.forEach(cat => {
         categoryContainer.innerHTML += `
-            <li id="${cat.id}" class="font-medium text-lg py-3 hover:border-b-4 hover:border-b-red-800 border-b-red-800">${cat.title}</li>
+            <li id="${cat.id}" class="font-medium text-lg py-3 hover:border-b-4 hover:border-b-red-800 border-b-red-800 ${cat.id=== 'main' ? 'border-b-4': ''}">${cat.title}</li>
             `
     });
 
@@ -52,19 +52,17 @@ const showNewsByCategory = (articles)=>{
     console.log(articles)
     articles.forEach(article =>{
         newsContainer.innerHTML+=`
-        <div>
+        <div class="border border-gray-200 rounded-lg">
 
 
-        <img src='${article.image.srcset[5].url}'
-        <h1>${article.title}</h1>
+        <img class="w-full rounded-t-lg" src='${article.image.srcset[5].url}'>
 
-        <p>${article.time}</P>
-        
-        
+        <div class="px-1 py-2">
+        <h1 class="font-bold">${article.title}</h1>
+        <p class="text-sm text-gray-800">${article.time}</P>
         </div>
         `
     })
 }
-{/* <div></div> */}
 loadCategory()
 loadNewsByCategory('main')
