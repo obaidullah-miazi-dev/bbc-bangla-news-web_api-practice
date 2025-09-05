50430de55ab9c8ef3bb4f55128ad3b9b5bdbacf4
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-05 17:54:21 +0600

    news category section added with toggling feature and functionality

--- script/script.js
+++ script/script.js
@@ -1,23 +1,47 @@
-// categories section api 
+const categoryContainer = document.getElementById('category-container')
+const loadCategory = () => {
+    const url = "https://news-api-fs.vercel.app/api/categories"
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const categories = data.categories
+            showCategory(categories);
+        })
 
-const url = "https://news-api-fs.vercel.app/api/categories"
-fetch(url)
-    .then(res => res.json())
-    .then(data => getCategories(data.categories))
+        .catch((err) => {
+            console.log(err)
+        })
 
-// get categories
-const getCategories = (categories) => {
-    const catContainer = document.getElementById('cat-container')
-    categories.forEach(cat => {
-        const catTitle = cat.title
-        const newCat = document.createElement('p')
-        newCat.innerHTML= `
-     <p class="py-2 font-medium text-xl cursor-pointer border-red-800 hover:border-b-4">
-                        ${catTitle}
-                    </p>
-
-    `
-
-        catContainer.append(newCat)
-    });
 }
+
+const showCategory = (categories) => {
+    categories.forEach(cat => {
+        categoryContainer.innerHTML += `
+            <li id="${cat.id}" class="font-medium text-lg py-3 hover:border-b-4 hover:border-b-red-800 border-b-red-800">${cat.title}</li>
+            `
+    });
+
+    categoryContainer.addEventListener('click', (e) => {
+
+        const allLi = document.querySelectorAll('li')
+        allLi.forEach(li => {
+            li.classList.remove('border-b-4')
+        })
+
+        if (e.target.localName === 'li') {
+            e.target.classList.add('border-b-4')
+        }
+
+        loadNewsByCategory(e.target.id)
+    })
+}
+
+const loadNewsByCategory = (categoryId) => {
+    fetch(`https://news-api-fs.vercel.app/api/categories/${categoryId}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+}
+
+loadCategory()
